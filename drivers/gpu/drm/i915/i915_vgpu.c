aaa919534e54f5d5d883a2bf81a526c8e553961f
Lu,Yang A <yang.a.lu@intel.com>
2020-07-24 01:47:49 +0800

    disable_pv_4

--- drivers/gpu/drm/i915/i915_vgpu.c
+++ drivers/gpu/drm/i915/i915_vgpu.c
@@ -99,9 +99,10 @@ void intel_detect_vgpu(struct drm_i915_private *dev_priv)
 	/* guest driver PV capability */
 	DRM_INFO("Set pv_caps=0.\n");
 	dev_priv->vgpu.pv_caps = 0;
+	dev_priv->vgpu.pv_caps |= PV_PPGTT;
 	//dev_priv->vgpu.pv_caps = PV_PPGTT | PV_GGTT;
 	//dev_priv->vgpu.pv_caps |= PV_SUBMISSION | PV_HW_CONTEXT;
-	dev_priv->vgpu.pv_caps |= PV_INTERRUPT;
+	//dev_priv->vgpu.pv_caps |= PV_INTERRUPT;
 
 	if (!intel_vgpu_check_pv_caps(dev_priv, shared_area)) {
 		DRM_INFO("Virtual GPU for Intel GVT-g detected.\n");
