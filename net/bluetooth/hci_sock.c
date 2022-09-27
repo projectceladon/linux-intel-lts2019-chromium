09bcfebd27d3813ac0f999f0f000f32f5bb559f6
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2022-09-27 09:58:15 -0700

    FROMGIT: Bluetooth: Prevent double register of suspend
    Suspend notifier should only be registered and unregistered once per
    hdev. Simplify this by only registering during driver registration and
    simply exiting early when HCI_USER_CHANNEL is set.
    
    Reported-by: syzbot <syzkaller@googlegroups.com>
    Fixes: 359ee4f834f5 (Bluetooth: Unregister suspend with userchannel)
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    (cherry picked from commit 4b8af331bb4d4cc8bb91c284b11b98dd1e265185
     git://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git master)
    
    BUG=b:243830809
    TEST=Suspend stress on Zork
    
    Change-Id: I74ca17867330be4e00878a546bf4f202ad24eb16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3927828
    Tested-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Reviewed-by: Michael Sun <michaelfsun@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@google.com>

--- net/bluetooth/hci_sock.c
+++ net/bluetooth/hci_sock.c
@@ -878,7 +878,6 @@ static int hci_sock_release(struct socket *sock)
 			 */
 			hci_dev_do_close(hdev);
 			hci_dev_clear_flag(hdev, HCI_USER_CHANNEL);
-			hci_register_suspend_notifier(hdev);
 			mgmt_index_added(hdev);
 		}
 
@@ -1195,7 +1194,6 @@ static int hci_sock_bind(struct socket *sock, struct sockaddr *addr,
 		}
 
 		mgmt_index_removed(hdev);
-		hci_unregister_suspend_notifier(hdev);
 
 		err = hci_dev_open(hdev->id);
 		if (err) {
@@ -1210,7 +1208,6 @@ static int hci_sock_bind(struct socket *sock, struct sockaddr *addr,
 				err = 0;
 			} else {
 				hci_dev_clear_flag(hdev, HCI_USER_CHANNEL);
-				hci_register_suspend_notifier(hdev);
 				mgmt_index_added(hdev);
 				hci_dev_put(hdev);
 				goto done;
