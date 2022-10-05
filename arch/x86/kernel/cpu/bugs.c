76f5389efff9c9b075f23e3c3615d31a58262dd5
Suleiman Souhlal <suleiman@google.com>
2022-10-05 23:02:34 +0900

    Revert "BACKPORT: FROMGIT: x86/bugs: Report AMD retbleed vulnerability"
    This reverts commit 2ff999433496679b9f522145aea669c0bfc8566b.
    
    Revert so that we can take LTS backports instead.
    
    BUG=b:239048801
    TEST=Booted.
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    Change-Id: If5c70a00451a9d90ec8285e6ac31664513c8e4e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3936476
    Commit-Queue: Suleiman Souhlal <suleiman@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Tested-by: Suleiman Souhlal <suleiman@chromium.org>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -1977,11 +1977,6 @@ static ssize_t srbds_show_state(char *buf)
 	return sprintf(buf, "%s\n", srbds_strings[srbds_mitigation]);
 }
 
-static ssize_t retbleed_show_state(char *buf)
-{
-	return sprintf(buf, "Vulnerable\n");
-}
-
 static ssize_t cpu_show_common(struct device *dev, struct device_attribute *attr,
 			       char *buf, unsigned int bug)
 {
@@ -2027,9 +2022,6 @@ static ssize_t cpu_show_common(struct device *dev, struct device_attribute *attr
 	case X86_BUG_MMIO_STALE_DATA:
 	case X86_BUG_MMIO_UNKNOWN:
 		return mmio_stale_data_show_state(buf);
-
-	case X86_BUG_RETBLEED:
-		return retbleed_show_state(buf);
 
 	default:
 		break;
@@ -2089,11 +2081,6 @@ ssize_t cpu_show_mmio_stale_data(struct device *dev, struct device_attribute *at
 		return cpu_show_common(dev, attr, buf, X86_BUG_MMIO_UNKNOWN);
 	else
 		return cpu_show_common(dev, attr, buf, X86_BUG_MMIO_STALE_DATA);
-}
-
-ssize_t cpu_show_retbleed(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	return cpu_show_common(dev, attr, buf, X86_BUG_RETBLEED);
 }
 #endif
 
