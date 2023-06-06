b507a9eee36a298f247b0fbb8caebd38b20a8b6e
Ricardo Ribalda <ribalda@chromium.org>
2023-06-06 17:00:18 +0000

    CHROMIUM: media: uvcvideo: Fix Disable Exposure Auto Priority control for Microdia Integrated_Webcam_HD
    The index for UVC_CT_AE_PRIORITY_CONTROL is 2 not 3.
    
    BUG=b:272738845
    UPSTREAM-TASK=b:255969015
    TEST=v4l2-compliance
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    Change-Id: I128dd9112b95f52bc1e1ca1d03e7a3c7ec1f1f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4594657
    Reviewed-by: Yunke Cao <yunkec@chromium.org>

--- drivers/media/usb/uvc/uvc_ctrl.c
+++ drivers/media/usb/uvc/uvc_ctrl.c
@@ -2384,7 +2384,7 @@ static void uvc_ctrl_prune_entity(struct uvc_device *dev,
 	};
 	static const struct uvc_ctrl_blacklist camera_blacklist[] = {
 		/* Microdia Integrated_Webcam_HD, Exposure Auto Priority */
-		{ { USB_DEVICE(0x0c45, 0x6a14) }, 3 },
+		{ { USB_DEVICE(0x0c45, 0x6a14) }, 2 },
 		{ { USB_DEVICE(0x06f8, 0x3005) }, 9 }, /* Zoom, Absolute */
 		/* Region of interest(ROI) auto control */
 		/* Chicony Electronics Co., Ltd HD User Facing */
