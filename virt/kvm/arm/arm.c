ca96a85dce126cb496f252da8d790be225e5fa98
David Dai <davidai@google.com>
2023-08-25 10:10:25 +0900

    CHROMIUM: BACKPORT: FROMLIST: kvm: arm64: Add support for get_cur_cpufreq service
    This service allows guests to query the host for frequency of the CPU
    that the vCPU is currently running on.
    
    Co-developed-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: David Dai <davidai@google.com>
    (am from https://patchwork.kernel.org/patch/13194986/)
    (also found at https://lore.kernel.org/r/20230330224348.1006691-3-davidai@google.com)
    
    Conflicts:
       Documentation/virt/kvm/api.rst
       Documentation/virt/kvm/index.rst
       arch/arm64/include/uapi/asm/kvm.h
       arch/arm64/kvm/arm.c -> virt/kvm/arm/arm.c
       arch/arm64/kvm/hypercalls.c -> virt/kvm/arm/hypercalls.c
       include/uapi/linux/kvm.h
       include/linux/arm-smccc.h
       tools/arch/arm64/include/uapi/asm/kvm.h
    
    BUG=b:249685960, b:297154857
    TEST=Build
    UPSTREAM-TASK=b:256983979
    
    Change-Id: I6a3a8db6c90c8b39efb53ebfd230c5089ca8109e
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4813145
    Commit-Queue: Suleiman Souhlal <suleiman@chromium.org>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>

--- virt/kvm/arm/arm.c
+++ virt/kvm/arm/arm.c
@@ -201,6 +201,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_IMMEDIATE_EXIT:
 	case KVM_CAP_VCPU_EVENTS:
 	case KVM_CAP_ARM_IRQ_LINE_LAYOUT_2:
+	case KVM_CAP_GET_CUR_CPUFREQ:
 		r = 1;
 		break;
 	case KVM_CAP_ARM_SET_DEVICE_ADDR:
